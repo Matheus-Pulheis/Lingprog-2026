afa6614421a72f03da7d4fe77e0ea2e911c67dd1
MATHEUS PULHEIS DOS REIS <matheusbrasileiroreis@gmail.com>
2026-02-23 10:39:04 -0300

    imc

---
+++ Ex01/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main{
+    public static void main(String[] args){
+
+        float altura = 1.81F;
+        float peso = 72;
+        float IMC = peso / (altura * altura);
+        System.out.print(f"seu IMC é igual a "+ IMC);
+    }
+}
